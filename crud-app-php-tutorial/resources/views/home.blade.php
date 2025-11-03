4d0d3700d24644a3ae5e88f6650f601a9d8d02c6
Ravindu Gunawardana <ravindugunawardana7@gmail.com>
2025-11-03 12:36:24 +0530

    Add home view with authentication forms for login and registration

---
+++ crud-app-php-tutorial/resources/views/home.blade.php
@@ -0,0 +1,42 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    @auth
+    <p>Congrats you are logged in</p>
+    <form action="/logout" method="POST">
+    @csrf
+    <button>Log Out</button>
+    </form>
+
+    @else
+        <div style="border: 3px solid black;">
+            <h1">Register</h1>
+                <form action="/register" method="POST">
+                    @csrf
+                    <input name="name" type="text" placeholder="name" style="margin: 3px;">
+                    <input name="email" type="text" placeholder="email">
+                    <input name="password" type="password" placeholder="password">
+                    <button>Register</button>
+                </form>
+        </div>
+
+        <div style="border: 3px solid black;">
+            <h1">Login</h1>
+                <form action="/login" method="POST">
+                    @csrf
+                    <input name="loginname" type="text" placeholder="name" style="margin: 3px;">
+                    <input name="loginpassword" type="password" placeholder="password">
+                    <button>Login</button>
+                </form>
+        </div>
+
+    @endauth
+</body>
+</html>
